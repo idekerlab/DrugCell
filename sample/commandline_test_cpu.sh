11f4388f7e6cba5d6db50ec1ee58d1294698f89a
Jisoo Park <jp.jisoopark@gmail.com>
2020-04-08 11:48:29 -0700

    commit before manuscript submission

--- sample/commandline_test_cpu.sh
+++ sample/commandline_test_cpu.sh
@@ -8,12 +8,13 @@ testdatafile=$inputdir"drugcell_test.txt"
 mutationfile=$inputdir"cell2mutation.txt"
 drugfile=$inputdir"drug2fingerprint.txt"
 
-#modelfile="../pretrained_model/model_100"
-modelfile="MyModel/model_50"
+modelfile="../pretrained_model/drugcell_v1.pt"
 resultdir="Result_sample"
 hiddendir="Hidden_sample"
 
 mkdir $resultdir
 mkdir $hiddendir
+
+source activate pytorch3drugcell
 
 python -u ../code/predict_drugcell_cpu.py -gene2id $gene2idfile -cell2id $cell2idfile -drug2id $drug2idfile -genotype $mutationfile -fingerprint $drugfile -hidden $hiddendir -result $resultdir -predict $inputdir/drugcell_test.txt -load $modelfile > test_sample.log
--- sample/commandline_train.sh
+++ sample/commandline_train.sh
@@ -15,5 +15,7 @@ cudaid=0
 modeldir=Model_sample
 mkdir $modeldir
 
+source activate pytorch3drugcell
+
 python -u ../code/train_drugcell.py -onto $ontfile -gene2id $gene2idfile -drug2id $drug2idfile -cell2id $cell2idfile -train $traindatafile -test $valdatafile -model $modeldir -cuda $cudaid -genotype $mutationfile -fingerprint $drugfile -genotype_hiddens 6 -drug_hiddens '100,50,6' -final_hiddens 6 -epoch 100 -batchsize 5000 > train_sample.log
 
