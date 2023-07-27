0a5cc0de6872cc096d7d224f0f96e7bc22e19eec
Rohan Gnanaolivu <ac.rgnanaolivu@lambda0.cels.anl.gov>
2023-07-27 08:36:24 -0700

    updated file for HPO

--- cfg-my-experiment-1.sh
+++ cfg-my-experiment-1.sh
@@ -3,7 +3,8 @@
 
 source_cfg -v cfg-my-settings.sh
 
+export CANDLE_DATA_DIR=/tmp/rgnanaolivu
 export CANDLE_MODEL_TYPE="SINGULARITY"
 export PARAM_SET_FILE=my-nt3-search.json
 export MODEL_NAME=/software/improve/images/DrugCell.sif
-export CANDLE_DATA_DIR=Data
+
